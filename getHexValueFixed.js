0940117dfe5c9e112b68e1759cab117feac707a7
cookiemovies <martin@kochloefl.de>
2020-11-23 22:55:03 +0100

    mssql added

---
+++ getHexValueFixed.js
@@ -0,0 +1,29 @@
+var readline = require('readline');
+
+process.stdin.resume();
+
+var r = readline.createInterface(process.stdin, process.stdout);
+
+var colorHexes = {
+    red: '#FF0000',
+    yellow: '#FFFF00',
+    blue: '#0000FF',
+    black: '#000000',
+    white: '#FFFFFF',
+    green: '#008000',
+    purple: '#800080',
+    orange: '#FFA500'
+};
+
+r.question('Enter a color to get its hex value (e.x. blue): ', function (statement) {
+  if (statement == '') {
+    statement = 'no';
+  }
+  statement = statement.toLowerCase();
+    if (statement && colorHexes[statement]) {
+      console.log(colorHexes[statement]);
+    } else {
+      console.log('Can\'t find that color in the system, try Googling it!')
+    }
+    process.stdin.pause();
+});
