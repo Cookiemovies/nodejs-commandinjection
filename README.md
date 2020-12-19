1e964f3f04d4d5ff4d2c714dd0099aaee5469bbb
cookiemovies <martin@kochloefl.de>
2020-12-19 17:55:37 +0100

    readme and other changes

--- README.md
+++ README.md
@@ -22,38 +22,33 @@ http://localhost:3000/sql?price=50;SELECT%20*%20FROM%20PRODUCTS
 
 # Install and run
 
-To install this repository on your computer youe need Node.js V12 installed 
-
-Run:  
-
+To install this repository on your computer you need Node.js.  
+Before you can run it you need SQL Server / Express installed as well and the Northwind database imported.  
+  
 ```
 git clone https://github.com/Cookiemovies/nodejs-commandinjection  
 npm install  
 ```
 
-In order to see the Contrast Security results you need to create your contrast_security.yaml file  
+## Install SQL Server Express, Management Studio and the Northwind database
 
-Then run: 
+Download SQL Server and Management Studio from the Microsoft website.
+Google for the Northwind database and find an Export
+Import the database at command line or in Management Studio
+Setup the database with your credentials.
 
-```
-npm install @contrast/agent 
-```
+MS SQL Server Express can be found here:
+https://www.microsoft.com/en-us/download/details.aspx?id=101064  
+  
+The Northwind database example for import can be found here:
+https://github.com/Microsoft/sql-server-samples/tree/master/samples/databases/northwind-pubs
+  
+SQL Server Management Studio can be downloaded here:  
+https://go.microsoft.com/fwlink/?linkid=2147207&clcid=0x409  
 
-For running the nodejs/express application:  
-
+## Run
+  
 ```
 npm start  
 ```
-
-For running the nodejs/express application including the Contrast Agent:  
-
-```
-npm contrast  
-```
-
-# Result from Contrast Security regarding code injection
-
-![Contrast Security](command-injection-outfile.jpg)
-
-The 2 high security issues are in /outfile.
-In /outsafe the issues are resolved and you have a secure application.
+  
--- simpleServer.js
+++ simpleServer.js
@@ -1,166 +1,210 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-const child_process = require('child_process');
+const child_process = require("child_process");
 app.disable("x-powered-by");
 
-const htmlbody = '<html><body>';
-const bodyhtml = '</body></html>';
+const htmlbody = "<html><body>";
+const bodyhtml = "</body></html>";
 const homepage = '<p><a href="http://localhost:3000/">BACK</a></p>';
-const welcome = '<h1>Welcome to the code and SQL injection test</h1>';
+const welcome = "<h1>Welcome to the code and SQL injection test</h1>";
 const novalidation = "Filename could not be validated: ";
-const fileoutput = 'Will output a file to console: ';
-const possible = 'Possible Command Injection! ';
-const problem = '<p>Problem: <a href="http://localhost:3000/outfile?filename=outputfile.txt|ls -a">http://localhost:3000/outfile?filename=outputfile.txt|ls -a</a></p>';
-const safe1   = '<p>Safe 1: <a href="http://localhost:3000/outsafe?filename=outputfile.txt|ls%20-a">http://localhost:3000/outsafe?filename=outputfile.txt|ls -a</a></p>';
-const safe2   = '<p>Safe 2: <a href="http://localhost:3000/outsafe?filename=outputfile.txt">http://localhost:3000/outsafe?filename=outputfile.txt</a></p>';
-const sql     = '<p>SQL: <a href="http://localhost:3000/sql?price=50">http://localhost:3000/sql?price=50</a></p>';
-const sqlinj  = '<p>SQL Injection: <a href="http://localhost:3000/sql?price=50;SELECT * FROM PRODUCTS">http://localhost:3000/sql?price=50;SELECT * FROM PRODUCTS</a></p>';
+const fileoutput = "Will output a file to console: ";
+const possible = "Possible Command Injection! ";
+const problem =
+  '<p>Problem: <a href="http://localhost:3000/outfile?filename=outputfile.txt|ls -a">http://localhost:3000/outfile?filename=outputfile.txt|ls -a</a></p>';
+const safe1 =
+  '<p>Safe 1: <a href="http://localhost:3000/outsafe?filename=outputfile.txt|ls%20-a">http://localhost:3000/outsafe?filename=outputfile.txt|ls -a</a></p>';
+const safe2 =
+  '<p>Safe 2: <a href="http://localhost:3000/outsafe?filename=outputfile.txt">http://localhost:3000/outsafe?filename=outputfile.txt</a></p>';
+const sql =
+  '<p>SQL: <a href="http://localhost:3000/sql?price=50">http://localhost:3000/sql?price=50</a></p>';
+const sqlinj =
+  '<p>SQL Injection: <a href="http://localhost:3000/sql?price=50;SELECT * FROM PRODUCTS">http://localhost:3000/sql?price=50;SELECT * FROM PRODUCTS</a></p>';
 
-app.get('/', function (req, res) {
-  res.send(htmlbody + welcome + problem + safe1 + safe2 + sql + sqlinj + bodyhtml)
-})
-
-app.get('/outfile', function (req, res) {
-
-  child_process.exec(
-      'cat ' + req.query.filename, (error, stdout, stderr) => {
-        if (error) {
-        console.log('error: ', error.messsage);
-        return;
-      }
-      if (stderr) {
-        console.log('stderr: ', stderr)
-        return;
-      }
-      console.log('OUTPUT:\r\n', stdout);
-    });
-  res.send(htmlbody + '<h1>' + possible + '</h1><h1>' + fileoutput + req.query.filename + '</h1>' + homepage + bodyhtml)
+app.get("/", function (req, res) {
+  res.send(
+    htmlbody + welcome + problem + safe1 + safe2 + sql + sqlinj + bodyhtml
+  );
 });
 
-app.get('/outsafe', function (req, res) {
+app.get("/outfile", function (req, res) {
+  child_process.exec("cat " + req.query.filename, (error, stdout, stderr) => {
+    if (error) {
+      console.log("error: ", error.messsage);
+      return;
+    }
+    if (stderr) {
+      console.log("stderr: ", stderr);
+      return;
+    }
+    console.log("OUTPUT:\r\n", stdout);
+  });
+  res.send(
+    htmlbody +
+      "<h1>" +
+      possible +
+      "</h1><h1>" +
+      fileoutput +
+      req.query.filename +
+      "</h1>" +
+      homepage +
+      bodyhtml
+  );
+});
 
+app.get("/outsafe", function (req, res) {
   const filename = req.query.filename;
 
-  if  (
-      (filename.includes(",")) 
-      || 
-      (filename.includes(";"))
-      || 
-      (filename.includes("&"))
-      || 
-      (filename.includes("|"))
-      ) {
-        res.send(htmlbody + '<h1>' + novalidation + encodeURI(filename) + ' </h1>' + homepage + bodyhtml);
-        console.log(novalidation + filename);
+  if (
+    filename.includes(",") ||
+    filename.includes(";") ||
+    filename.includes("&") ||
+    filename.includes("|")
+  ) {
+    res.send(
+      htmlbody +
+        "<h1>" +
+        novalidation +
+        encodeURI(filename) +
+        " </h1>" +
+        homepage +
+        bodyhtml
+    );
+    console.log(novalidation + filename);
     return;
   }
 
-  child_process.execFile(
-      'cat', [filename], (error, stdout, stderr) => {
-        if (error) {
-        console.log('error: ', error.messsage)
-        return;
-      }
-      if (stderr) {
-        console.log('stderr: ', stderr)
-        return;
-      }
-      console.log('OUTPUT:\r\n', stdout);
-    });
-  res.send(htmlbody + '<h1>' + fileoutput + encodeURI(filename) + ' </h1>' + homepage + bodyhtml) ;
+  child_process.execFile("cat", [filename], (error, stdout, stderr) => {
+    if (error) {
+      console.log("error: ", error.messsage);
+      return;
+    }
+    if (stderr) {
+      console.log("stderr: ", stderr);
+      return;
+    }
+    console.log("OUTPUT:\r\n", stdout);
+  });
+  res.send(
+    htmlbody +
+      "<h1>" +
+      fileoutput +
+      encodeURI(filename) +
+      " </h1>" +
+      homepage +
+      bodyhtml
+  );
 });
 
-
 //MSSQL Connection
-var Connection = require('tedious').Connection;  
-var Request = require('tedious').Request;  
-var TYPES = require('tedious').TYPES;  
+var Connection = require("tedious").Connection;
+var Request = require("tedious").Request;
+var TYPES = require("tedious").TYPES;
 
 // const servername = 'COOKIE-LAPTOP\SQLEXPRESS';
-const servername = 'localhost';
-const database = 'Northwind';
-const user = 'node';
-const pass = 'node';
+const servername = "localhost";
+const database = "Northwind";
+const user = "node";
+const pass = "node";
 
-app.get('/sql', function (req, res) {
-
-  var config = {  
-      server: servername,
-      authentication: {
-          type: 'default',
-          options: {
-              userName: user,
-              password: pass
-          }
-      },
+app.get("/sql", function (req, res) {
+  var config = {
+    server: servername,
+    authentication: {
+      type: "default",
       options: {
-          encrypt: false,
-          database: database,
-          validateBulkLoadParameters: false
-      }
-  }; 
+        userName: user,
+        password: pass,
+      },
+    },
+    options: {
+      encrypt: false,
+      database: database,
+      validateBulkLoadParameters: false,
+    },
+  };
 
-  var connection = new Connection(config);  
+  var connection = new Connection(config);
 
-  var htmlhead = '<html><head><style>table { width:400pt; } table, th, td {border: 1px solid black; border-collapse: collapse; } th, td { padding: 15px ; text-align: middle; } #t01 tr:nth-child(even) { background-color: #eee; } #t01 tr:nth-child(odd) { background-color: #fff; } #t01 th { background-color: black; color: white; }</style></head><body>';
-  var resultrows = '<table id="t01"><tr><th>ID</th><th>Product</th><th>UnitPrice</th></tr>' ;
+  var htmlhead =
+    "<html><head><style>table { width:400pt; } table, th, td {border: 1px solid black; border-collapse: collapse; } th, td { padding: 15px ; text-align: middle; } #t01 tr:nth-child(even) { background-color: #eee; } #t01 tr:nth-child(odd) { background-color: #fff; } #t01 th { background-color: black; color: white; }</style></head><body>";
+  var resultrows =
+    '<table id="t01"><tr><th>ID</th><th>Product</th><th>UnitPrice</th></tr>';
 
-  connection.connect(function(err) {  
-      if (err) {
-        console.log('ERROR IN LOGIN:' + err);
-        res.send('<html><body><h1>ERROR IN LOGIN: ' + err + '</h1></body></html>');
-      }
-      else {
-        var price = req.query.price ;
-        if (price == undefined) {
-          readDB('50');
+  connection.connect(function (err) {
+    if (err) {
+      console.log("ERROR IN LOGIN:" + err);
+      res.send(
+        "<html><body><h1>ERROR IN LOGIN: " + err + "</h1></body></html>"
+      );
+    } else {
+      var price = req.query.price;
+
+      /*
+        var reg = new RegExp('^[0-9]+$');
+        if (!(reg.test(price)))
+        {
+          price = '40';
         }
-        else {
-          readDB(price);
-        }
-      }
-  });  
+        */
 
-  connection.on('debug', function(text) {
-    console.log('DEBUG MESSAGE: ' + text);
+      if (price == undefined) {
+        readDB("50");
+      } else {
+        readDB(price);
+      }
+    }
   });
 
-  function readDB(UnitPrice) {  
-    var request = new Request('SELECT ProductID, ProductName, UnitPrice FROM Products WHERE UnitPrice > ' + UnitPrice + ';', function(err, rowCount) {  
-      if (err) {  
-          console.log('ERROR IN REQUEST:' + err);
-      }  
-      else { 
-        console.log('Returned ' + rowCount + ' rows');
+  connection.on("debug", function (text) {
+    console.log("DEBUG MESSAGE: " + text);
+  });
+
+  function readDB(UnitPrice) {
+    var request = new Request(
+      "SELECT ProductID, ProductName, UnitPrice FROM Products WHERE UnitPrice > " +
+        UnitPrice +
+        ";",
+      function (err, rowCount) {
+        if (err) {
+          console.log("ERROR IN REQUEST:" + err);
+        } else {
+          console.log("Returned " + rowCount + " rows");
+        }
+        connection.close();
+        res.send(
+          htmlhead +
+            "<h1>Northwind Products</h1>" +
+            resultrows +
+            "</table>" +
+            bodyhtml
+        );
       }
-      connection.close();
-      res.send(htmlhead + '<h1>Northwind Products</h1>' + resultrows + '</table>' + bodyhtml);
-    });  
-    
-var result = "<tr>";  
+    );
 
-    console.log('REQUEST ON GETROWS');
-    request.on('row', function(columns) {  
-      columns.forEach(function(column) {  
-        if (column.value === null) {  
-          console.log('NULL');  
-        } 
-        else {  
-          result+= '<td>' + column.value + '</td>';  
-        }  
-      });  
-      resultrows += result + '</tr>';
-      result ='<tr>';  
-    });  
+    var result = "<tr>";
 
- request.on('done', function(rowCount, more) {  
-      console.log(rowCount + ' rows returned');  
-    });  
-  connection.execSql(request);  
-  }  
-});  
+    console.log("REQUEST ON GETROWS");
+    request.on("row", function (columns) {
+      columns.forEach(function (column) {
+        if (column.value === null) {
+          console.log("NULL");
+        } else {
+          result += "<td>" + column.value + "</td>";
+        }
+      });
+      resultrows += result + "</tr>";
+      result = "<tr>";
+    });
+
+    request.on("done", function (rowCount, more) {
+      console.log(rowCount + " rows returned");
+    });
+    connection.execSql(request);
+  }
+});
 
 app.listen(3000, function () {
-  console.log('Node app listening on port 3000!')
-})
+  console.log("Node app listening on port 3000!");
+});
